3cf673bef1376dfcd9b99d42e3da372fd8fb3841
Nguyen Nam <nam.hellfire@gmail.com>
2016-11-23 21:01:22 +0700

    Comment

--- app/src/main/java/com/example/switchwifi/listDialogCustom.java
+++ app/src/main/java/com/example/switchwifi/listDialogCustom.java
@@ -2,9 +2,6 @@ package com.example.switchwifi;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 /**
  * Created by nguyennam on 11/23/16.
@@ -21,25 +18,25 @@ public class listDialogCustom extends Dialog {
     private void showDialog() {
         final Dialog dialog = new Dialog(context);
 
-        View view = getLayoutInflater().inflate(R.layout.custom_list_dialog, null);
-
-        ListView lvListWifi = (ListView) view.findViewById(R.id.lvWifi);
-
-        // Change MyActivity.this and myListOfItems to your own values
-        ListWifiAdapter adapter = new ListWifiAdapter(context, myListOfItems);
-
-        lvListWifi.setAdapter(adapter);
-
-        lvListWifi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
-
-        dialog.setContentView(view);
-
-        dialog.show();
+//        View view = getLayoutInflater().inflate(R.layout.custom_list_dialog, null);
+//
+//        ListView lvListWifi = (ListView) view.findViewById(R.id.lvWifi);
+//
+//        // Change MyActivity.this and myListOfItems to your own values
+//        ListWifiAdapter adapter = new ListWifiAdapter(context, myListOfItems);
+//
+//        lvListWifi.setAdapter(adapter);
+//
+//        lvListWifi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//        });
+//
+//        dialog.setContentView(view);
+//
+//        dialog.show();
 
     }
 }
